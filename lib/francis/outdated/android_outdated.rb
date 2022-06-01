f9a3cf0147d7b29d767a89b697f4fbfea139c162
Marcin Siemaszko <siemman@gmail.com>
2022-06-01 11:35:48 +0200

    Specify the report name for android outdated.

--- lib/francis/outdated/android_outdated.rb
+++ lib/francis/outdated/android_outdated.rb
@@ -3,7 +3,7 @@
 def android_outdated_dependencies
   `mkdir $HOME/.gradle/init.d/`
   `cp #{__dir__}/scripts/add-versions-plugin.gradle $HOME/.gradle/init.d/add-versions-plugin.gradle`
-  `./gradlew dependencyUpdates -Drevision=release -DoutputFormatter=json -DoutputDir=.`
+  `./gradlew dependencyUpdates -Drevision=release -DoutputFormatter=json -DoutputDir=. -DreportfileName=report`
   raw_dependencies_report = File.read("report.json")
   dependencies = JSON.parse(raw_dependencies_report)
   total_dependencies_count = dependencies["count"]
