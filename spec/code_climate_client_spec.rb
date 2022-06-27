5aaf5e26cb6be5740987cef2f8984d9d4e65a19b
Marcin Siemaszko <marcin.siemaszko@netguru.pl>
2022-06-27 14:11:16 +0200

    Codeclimate fallback (#14)
    * Add fallback scenario for code climate

--- spec/code_climate_client_spec.rb
+++ spec/code_climate_client_spec.rb
@@ -20,4 +20,28 @@ describe CodeClimateClient do
     expect(result[:debt_value]).to eq(10.270369550217175)
     expect(result[:refactoring_time]).to eq(262.0)
   end
+
+  it "two" do
+    client = CodeClimateClient.new
+    client.repo_id = "fixture_repo_id"
+    client.token = "fixture_token"
+    client.branch = "master"
+    builds_response = Typhoeus::Response.new(code: 200, body: File.read("spec/fixtures/codeclimate_builds.json"))
+    snapshots_reponse = Typhoeus::Response.new(code: 200, body: File.read("spec/fixtures/codeclimate_snapshots.json"))
+    Typhoeus.stub(/snapshots/).and_return(snapshots_reponse)
+    count = 0
+    Typhoeus.stub(/builds/) do |request|
+      if count == 0
+        count += 1
+        Typhoeus::Response.new(code: 200)
+      else
+        builds_response
+      end
+    end
+    result = client.status
+    expect(result[:rating]).to eq("C")
+    expect(result[:issues]).to eq(6)
+    expect(result[:debt_value]).to eq(10.270369550217175)
+    expect(result[:refactoring_time]).to eq(262.0)
+  end
 end
